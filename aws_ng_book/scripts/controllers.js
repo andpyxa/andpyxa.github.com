c89c6d80d3f9341002768ddd1ce0fc410ca479d0
andpyxa <andpyxa@users.noreply.github.com>
2016-04-23 19:59:50 +0300

    Added files via upload

--- aws_ng_book/scripts/controllers.js
+++ aws_ng_book/scripts/controllers.js
@@ -1,7 +1,20 @@
 angular.module('myApp')
-    .controller('MainController', function ($scope) {
+    .controller('MainController', function ($scope, UserService) {
         $scope.signedIn = function (oauth) {
-            $scope.user = oauth;
+
+            //This method would be an ideal place to set a redirect to a new route
+            //(for instance, the /dashboard route for authenticated users).
+            //
+
+
+            //btDCGMshR_P1vBVvU492zdgt
+
+            UserService.setCurrentUser(oauth)
+                .then(function (user) {
+                    $scope.user = user;
+                });
+            console.log(oauth);
+            
         }
     });
 
